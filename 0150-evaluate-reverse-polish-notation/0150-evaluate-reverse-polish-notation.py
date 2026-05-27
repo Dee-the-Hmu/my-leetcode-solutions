d0204617bdebef9be86d2b81b27cfcb02e7f9ff8
Dee <aeinhmunaychi.ts@gmail.com>
2026-05-27 15:20:48 -0700

    Time: 11 ms (37.84%), Space: 13.7 MB (11.4%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,47 @@
+"""
+U: 
+    input: an array of strings "tokens" 
+    output: return int (value of the expression)
+
+    valid operations = + - * /
+    no division by zero 
+    no decimals (2.9 -> 2)
+
+
+M: stack (store numbers)
+P: 
+    1. create a stack s 
+    2. iterate tokens 
+        3. if numbers, add to stack 
+        4. else: pop=b, pop=a, do a operators b 
+            add back to the stack 
+    5. return pop
+
+"""
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+
+        for token in tokens: 
+            if token.isdigit() or token.lstrip('-').isdigit(): # count -numbers as digit (strip - and is the remainder a digit?)
+                stack.append(token)
+            else: 
+                b = int(stack.pop())
+                a = int(stack.pop())
+
+                if token == "+":
+                    stack.append(str(a + b))
+                elif token == "-":
+                    stack.append(str(a - b))
+                elif token == "*":
+                    stack.append(str(a * b))
+                elif token == "/":
+                    stack.append(str(int(float(a) / b))) #truncate towards zero 
+
+        return int(stack.pop())
+        
