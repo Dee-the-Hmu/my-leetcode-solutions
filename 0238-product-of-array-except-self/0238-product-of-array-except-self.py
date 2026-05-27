03562f9b4fc79c835c89ba2750296127973bdc65
Dee <aeinhmunaychi.ts@gmail.com>
2026-05-26 17:33:39 -0700

    Time: 47 ms (21.31%), Space: 21.3 MB (30.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,3 +1,33 @@
+"""
+U:
+    input = an array
+    output = an array where each element is the products of all elements from the input array except self
+
+    constraint: solve it in O(n) without using division 
+        2 <= nums.length <= 1000
+        -20 <= nums[i] <= 20
+
+M: for each element, it involves information of left and information of right, use 1 array, calculate left and use that array to calculate right and multiply with 
+P: 
+    1. create an array/list "lst"
+    2. iterate thru the nums, index i 
+        3. for each index i, 
+            if i-1 is out of bound 
+                lst[i] = 1
+            else
+                lst[i] = lst[i-1] * num[i-1]
+    lst now have all the information about left side 
+    3. reverse iterate thru nums, index j 
+        4. for each index j, 
+            if j+1 is out of bound 
+                right_project = 1
+            else 
+                right_project *= nums[j+1]
+            lst[j] *= right_product 
+
+    return lst
+"""
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
