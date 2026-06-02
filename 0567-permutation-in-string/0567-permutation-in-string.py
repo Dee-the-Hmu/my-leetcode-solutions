4f3fc268bd19e10d699c5d0dcca0f7cdc0a76cc8
Dee <aeinhmunaychi.ts@gmail.com>
2026-06-01 21:04:41 -0700

    Time: 35 ms (40.28%), Space: 12.4 MB (98.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,32 +2,25 @@
 U: 
     input: 2 strings "s1" and "s2" 
     output: boolean 
-M: 2 dict 
-P: 
-    1. create a dict, org_d, key = char in s1 and value = its count, update_needed = False
-    2. create another dict, curr_d = org_d.copy()
-    create right pointer = 0, left ptr = 0 
-    3. iterate s2, for each char #right pointer 
-        char = s[right]
-        4. if char in curr_d: 
-                while char in curr_d: 
-                char = s[right]
-                5. curr_d[char] -= 1 
-                    update_needed = True
-                6. if curr_d[char] == 0 
-                    7. del curr_d[char]
-                right += 1
-                7. if not curr_d:
-                    return True 
-            else: #not in d 
-                left += 1
-                right = left
-                continue 
-        right += 1
-        8. if update_needed: 
-            curr_d = org_d.copy()
-            update_needed = False
+M: 2 pointers and 2 dict ***make use of len(s1) a valid permutation must have exactly that len, if not, increment left 
 
+P: 
+    1. create 2 dict, org_dict key = char of s1, val = its count
+        curr_dict
+    2. left ptr, right ptr starts at 0 
+    3. while right < len(s2) 
+        right_char = s2[right]
+        curr_d[right_char] = curr_d.get(right_char, 0) + 1 
+        if (right - left + 1) > len(s1) *****
+            left_char = s2[left]
+            if left_char in d: #safe check
+                d[left_char] -= 1 
+            if d[left_char] == 0
+                del d[left char]
+            left += 1 
+        if org_dict == curr_dict: 
+            return True 
+        right += 1
     return False 
 """
 
@@ -39,40 +32,29 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        org_d = {}
-        d = {}
-        update_needed = False
-        right = 0
-        left = 0
+        org_dict = {}
+        curr_d = {}
+        left, right = 0, 0
 
         for char in s1: 
-            org_d[char] = org_d.get(char, 0) + 1 
+            org_dict[char] = org_dict.get(char, 0) + 1 
 
-        d = org_d.copy()
+        while right < len(s2):
+            right_char = s2[right]
+            curr_d[right_char] = curr_d.get(right_char, 0) + 1 
 
-        while left <= right and right < len(s2):
-            char = s2[right]
-
-            if char in d: 
-                while right < len(s2) and char in d: 
-                    d[char] -= 1 
-                    update_needed = True 
-                    if d[char] == 0:
-                        del d[char]
-                    
-                    if not d: 
-                        return True
-                    right += 1 
-                    if right < len(s2):
-                        char = s2[right]
-            
-            else: 
-                left += 1
-                right = left 
-                if update_needed: 
-                    d = org_d.copy()
-                    update_needed = False
-
+            if (right - left + 1) > len(s1): #valid window can't be longer than s1
+                left_char = s2[left]
+                if left_char in curr_d: 
+                    curr_d[left_char] -= 1 
+                    if curr_d[left_char] == 0:
+                        del curr_d[left_char]
+                left += 1 
+                
+            if curr_d == org_dict: 
+                return True 
+            right += 1
+                
         return False
             
 
